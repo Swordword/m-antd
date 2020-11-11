54808193ab2308313d68277f4004ccdb1aea0307
Swordword <massivejohn123@gmail.com>
2020-11-11 23:46:14 +0800

    menu 基础组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
 import Button, { ButtonType, ButtonSize } from './components/Button/button'
+import Menu from './components/Menu/menu'
+import MenuItem from './components/Menu/menuItem'
+
 const App = () => {
   return (
     <div>
+      <p>learn react</p>
+      <header>
+        <Menu defaultIndex={0} mode='vertical'>
+          <MenuItem>cool link</MenuItem>
+          <MenuItem>cool link</MenuItem>
+          <MenuItem>
+            <a href='https://github.com'>to github</a>
+          </MenuItem>
+        </Menu>
+      </header>
       <Button
         onClick={(e) => {
           e.preventDefault()
@@ -14,7 +27,11 @@ const App = () => {
       <Button autoFocus btnType={ButtonType.Primary} size={ButtonSize.Large}>
         Hello
       </Button>
-      <Button btnType={ButtonType.Link} href='https://github.com' target='_blank'>
+      <Button
+        btnType={ButtonType.Link}
+        href='https://github.com'
+        target='_blank'
+      >
         Link
       </Button>
     </div>
---
+++ src/components/Menu/subMenu.tsx
@@ -0,0 +1,13 @@
+import React, { useContext, FunctionComponentElement } from 'react'
+import classNames from 'classnames'
+import { MenuContext } from './menu'
+
+export interface SubMenuProps {
+  
+}
+
+const SubMenu = () => {
+  return <div></div>
+}
+
+export default SubMenu
